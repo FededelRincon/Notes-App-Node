6e62d2b3ee4c51451efe274f9ee0661dfad43fdf
“fededelrincon” <delrincon@gmail.com>
2021-01-12 19:49:44 -0300

    010- importing Your Own Files

---
+++ app.js
@@ -0,0 +1,14 @@
+const getNotes = require('./notes.js')
+
+//opcion simple
+console.log(getNotes())
+
+//opcion using a variable
+// const msg = getNotes()
+// console.log(msg)
+
+
+
+// const add = require('./utils.js')
+// const sum = add(4, -2)
+// console.log(sum)
