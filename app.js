a638c3538f6f82f3204236096e6d6556ede36e00
“fededelrincon” <delrincon@gmail.com>
2021-01-13 10:58:50 -0300

    015-Getting input from Users

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
-const msg = getNotes()
-console.log(msg)
+// const msg = getNotes()
+// console.log(msg)
 
 
-console.log(chalk.bold.inverse.green('Success!'));
+// console.log(chalk.bold.inverse.green('Success!'));
+
+// console.log(process.argv[2]) //el process.argv da 3 cosas, el path + lugar donde se corre la app + lo que va despues del llamado: ie node app.js Andrew
+
+const command = process.argv[2]
+
+console.log(process.argv)
+
+if(command === 'add') {
+  console.log('Adding note!')
+} else if (command === 'remove') {
+  console.log('Removing note!') 
+}
