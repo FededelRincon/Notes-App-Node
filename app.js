5fa8ce092317003d709e6b68a0523b4bba57867a
“fededelrincon” <delrincon@gmail.com>
2021-01-13 11:24:19 -0300

    016 Argument Parsing with Yargs: Part I

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-// const msg = getNotes()
-// console.log(msg)
+//Customize yargs version
+yargs.version('1.1.0')
+
+//Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function () {
+    console.log('Adding a new note!')
+  }
+})
+
+//Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log('Removing the note!')
+  }
+})
+
+//Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler: function () {
+    console.log('Listing out all notes!')
+  }
+})
+
+//Create read command
+yargs.command({
+  command: 'read',
+  describe: 'read a note',
+  handler: function () {
+    console.log('Reading a note!')
+  }
+})
 
 
-// console.log(chalk.bold.inverse.green('Success!'));
 
-// console.log(process.argv[2]) //el process.argv da 3 cosas, el path + lugar donde se corre la app + lo que va despues del llamado: ie node app.js Andrew
 
-const command = process.argv[2]
-
-console.log(process.argv)
-
-if(command === 'add') {
-  console.log('Adding note!')
-} else if (command === 'remove') {
-  console.log('Removing note!') 
-}
+console.log(yargs.argv)
